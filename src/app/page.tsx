8a612a9ee3aca23b5e5c4ee51f1fd723814d7006
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-11-25 17:31:17 +0200

    ShadCN & Branding
    - Installed ShadCN UI
    - Added Digi-fly brand colors
    - Tested shadcn and the colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
+import { Button } from '@/components/ui/button'
+
 export default function Home() {
   return (
     <div className='grid items-center justify-items-center min-h-screen p-8 gap-16 sm:p-20'>
-      <main className='flex flex-col gap-8 items-center sm:items-start'>
-        <h1 className='text-sky-500'>
+      <main className='flex flex-col gap-8 items-center sm:items-start p-20 bg-digifly-purple'>
+        <h1 className='text-digifly-green'>
           Welcome to Digital-Fly Frontend Challenge
         </h1>
+
+        <Button>Get Started</Button>
       </main>
     </div>
   )
